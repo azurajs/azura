bab14dac887b2907598a453c7b0c9db4381e410d
0xviny <vgalvaomarcelino@gmail.com>
2026-01-06 22:20:16 -0300

    Refactor index.ts to organize exports and improve module structure

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,6 +1,28 @@
+// Core Server & Router
 export * from "./infra/Server";
-export * from "./decorators/Route";
+export { Router } from "./infra/Router";
+
+// Decorators
+export * from "./decorators";
+
+// Middleware
 export * from "./middleware";
+
+// Types
 export * from "./types/http/request.type";
 export * from "./types/http/response.type";
+export * from "./types/common.type";
+export * from "./types/routes.type";
+export * from "./types/validations.type";
+
+// Configuration
 export * from "./shared/config/ConfigModule";
+
+// Utils
+export { logger } from "./utils/Logger";
+export { parseQS } from "./utils/Parser";
+export { HttpError } from "./infra/utils/HttpError";
+export { parseCookiesHeader } from "./utils/cookies/ParserCookie";
+export { serializeCookie } from "./utils/cookies/SerializeCookie";
+export { validateSchema } from "./utils/validators/SchemaValidator";
+export { validateDto, getDtoValidators } from "./utils/validators/DTOValidator";
